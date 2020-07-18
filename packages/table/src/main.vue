2c80021944610e5822f8fdc0c298d4e5c92c882e
Jonny <c1043870470@gmail.com>
2020-07-18 16:34:04 +0800

    fix: 修复table hidden

--- packages/table/src/main.vue
+++ packages/table/src/main.vue
@@ -2,53 +2,53 @@
   <div :class="{ 'el-card is-always-shadow': !noCard }">
     <ElTable ref="table" v-bind="tableProps" v-on="tableEvents">
       <template v-for="(item, tableIndex) in tableColumn">
-        <ElTableColumn v-if="item.slotName" :key="tableIndex" v-bind="item">
-          <template v-slot="{ row, column, $index }">
-            <slot
-              :index="$index"
-              :row="row"
-              :column="column"
-              :name="item.slotName"
-            />
-          </template>
-        </ElTableColumn>
-        <ElTableColumn
-          v-else-if="item.showTooltip"
-          :key="tableIndex"
-          v-bind="item"
-        >
-          <template v-slot="{ row, column, $index }">
-            <el-popover
-              trigger="hover"
-              placement="top"
-              popper-class="fnb-table__popper-class"
-              :disabled="!popoverDisabled[`${column.id}-index_${$index}`]"
-              :content="
-                item.formatter ? item.formatter(row, column) : row[item.prop]
-              "
-            >
-              <div
-                class="tow-line"
-                slot="reference"
-                @mouseenter.stop="
-                  handlePopover($event, `${column.id}-index_${$index}`)
-                "
-                @mouseleave.stop="
-                  handlePopover($event, `${column.id}-index_${$index}`)
+        <template v-if="!item.hidden">
+          <ElTableColumn v-if="item.slotName" :key="tableIndex" v-bind="item">
+            <template v-slot="{ row, column, $index }">
+              <slot
+                :index="$index"
+                :row="row"
+                :column="column"
+                :name="item.slotName"
+              />
+            </template>
+          </ElTableColumn>
+          <ElTableColumn
+            v-else-if="item.showTooltip"
+            :key="tableIndex"
+            v-bind="item"
+          >
+            <template v-slot="{ row, column, $index }">
+              <el-popover
+                trigger="hover"
+                placement="top"
+                popper-class="fnb-table__popper-class"
+                :disabled="!popoverDisabled[`${column.id}-index_${$index}`]"
+                :content="
+                  item.formatter ? item.formatter(row, column) : row[item.prop]
                 "
               >
-                {{
-                  item.formatter ? item.formatter(row, column) : row[item.prop]
-                }}
-              </div>
-            </el-popover>
-          </template>
-        </ElTableColumn>
-        <ElTableColumn
-          v-else-if="!item.hidden"
-          :key="tableIndex"
-          v-bind="item"
-        />
+                <div
+                  class="tow-line"
+                  slot="reference"
+                  @mouseenter.stop="
+                    handlePopover($event, `${column.id}-index_${$index}`)
+                  "
+                  @mouseleave.stop="
+                    handlePopover($event, `${column.id}-index_${$index}`)
+                  "
+                >
+                  {{
+                    item.formatter
+                      ? item.formatter(row, column)
+                      : row[item.prop]
+                  }}
+                </div>
+              </el-popover>
+            </template>
+          </ElTableColumn>
+          <ElTableColumn v-else :key="tableIndex" v-bind="item" />
+        </template>
       </template>
     </ElTable>
     <div
--- types/table.d.ts
+++ types/table.d.ts
@@ -1,7 +1,93 @@
-import { ElTableColumn } from 'element-ui/types/table-column'
+import { CreateElement, VNode } from 'vue'
+import {
+  ElTableColumn,
+  TableColumnType,
+  TableColumnFixedType,
+  RenderHeaderData,
+  SortOrders,
+  TableColumn,
+  TableColumnFilter
+} from 'element-ui/types/table-column'
 import { ElTable } from 'element-ui/types/table'
+import { ElementUIHorizontalAlignment } from 'element-ui/types/component'
+import { PopoverPlacement } from 'element-ui/types/popover'
 
-export interface FnbTableColumn extends Partial<ElTableColumn> {
+export interface FnbTableColumn<R = object> {
+  /** Type of the column. If set to `selection`, the column will display checkbox. If set to `index`, the column will display index of the row (staring from 1). If set to `expand`, the column will display expand icon. */
+  type?: TableColumnType
+
+  /** Column label */
+  label?: string
+
+  /** Column's key. If you need to use the filter-change event, you need this attribute to identify which column is being filtered */
+  columnKey?: string
+
+  /** Field name. You can also use its alias: property */
+  prop?: string
+
+  /** Column width */
+  width?: string | number
+
+  /** Column minimum width. Columns with `width` has a fixed width, while columns with `min-width` has a width that is distributed in proportion */
+  minWidth?: string | number
+
+  /** Whether column is fixed at left/right. Will be fixed at left if `true` */
+  fixed?: boolean | TableColumnFixedType
+
+  /** Render function for table header of this column */
+  renderHeader?: (h: CreateElement, data: RenderHeaderData) => VNode | string
+
+  /** Whether column can be sorted */
+  sortable?: boolean | 'custom'
+
+  /** Sorting method. Works when `sortable` is `true` */
+  sortMethod?: (a: any, b: any) => number
+
+  /** The order of the sorting strategies used when sorting the data. Works when `sortable` is `true`. */
+  sortOrders?: SortOrders[]
+
+  /** Whether column width can be resized. Works when border of `el-table` is `true` */
+  resizable?: boolean
+
+  /** Function that formats content */
+  formatter?: (row: R, column: TableColumn) => any
+
+  /** Whether to hide extra content and show them in a tooltip when hovering on the cell */
+  showOverflowTooltip?: boolean
+
+  /** Alignment */
+  align?: ElementUIHorizontalAlignment
+
+  /** Alignment of the table header. If omitted, the value of the `align` attribute will be applied */
+  headerAlign?: ElementUIHorizontalAlignment
+
+  /** Class name of cells in the column */
+  className?: string
+
+  /** Class name of the label of this column */
+  labelClassName?: string
+
+  /** Function that determines if a certain row can be selected, works when `type` is `'selection'` */
+  selectable?: (row: R, index: number) => boolean
+
+  /** Whether to reserve selection after data refreshing, works when `type` is `'selection'` */
+  reserveSelection?: boolean
+
+  /** An array of data filtering options */
+  filters?: TableColumnFilter[]
+
+  /** Placement for the filter dropdown */
+  filterPlacement?: PopoverPlacement
+
+  /** Whether data filtering supports multiple options */
+  filterMultiple?: Boolean
+
+  /** Data filtering method. If `filter-multiple` is on, this method will be called multiple times for each row, and a row will display if one of the calls returns `true` */
+  filterMethod?: (value: any, row: R) => boolean
+
+  /** Filter value for selected data, might be useful when table header is rendered with `render-header` */
+  filteredValue?: TableColumnFilter[]
+
   /** 该列是否是slot插槽（自定义列的内容，参数为 `{ row, column, index }`），插槽名 `name` 是 `slot` 字段值（当 `slot` 不为 `true`  时）或者 `prop` 字段值 */
   slot?: boolean | string
 
