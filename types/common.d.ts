f11c1792159b6f4034db2818fa81e0faf6aa128d
Jonny <c1043870470@gmail.com>
2021-03-16 15:57:01 +0800

    fix: render types

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,4 +1,11 @@
 import { VNode } from 'vue'
 
 /** 渲染函数返回值 */
-export type Render = JSX.Element | VNode | string | number | undefined | null
+export type Render =
+  | JSX.Element
+  | VNode
+  | string
+  | number
+  | undefined
+  | null
+  | Render[]
